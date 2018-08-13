8536b4afd52022a1e2637ea097529745b3140c2c
Luis Dasta <ldasta786@gmail.com>
2018-08-13 17:44:38 -0500

    quick test

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
 function onClick(){
-    const Gpio = require('onOff').Gpio;
-    const relay = new Gpio(14, 'out');
-    relay.writeSync(1);
-    relay.writeSync(0);
+    
     alert("Clicked!");
 }
