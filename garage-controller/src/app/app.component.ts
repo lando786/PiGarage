e6cf6e9e91ffd5dd9a254f9c3d9f9eab68063ef2
Luis Dasta <ldasta786@gmail.com>
2018-08-13 20:08:31 -0500

    page

--- garage-controller/src/app/app.component.ts
+++ garage-controller/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'garage-controller';
+  title = 'Garage Controller';
+  public clickCount = 1;
+  onClick() : void {
+    console.log('clicked');
+    this.clickCount = this.clickCount + 1;
+  }
 }
